8d286f4067961fc82ef6e5d28f93acacfa6db99d
Atsushi Togo <atz.togo@gmail.com>
2020-07-02 10:26:14 +0900

    Removed unnecessary files

--- conftest.py
+++ conftest.py
@@ -7,12 +7,3 @@ pytest_plugins = ['aiida.manage.tests.pytest_fixtures']
 @pytest.fixture(scope='function', autouse=True)
 def clear_database_auto(clear_database):  # pylint: disable=unused-argument
     """Automatically clear database in between tests."""
-
-
-@pytest.fixture(scope='function')
-def nims_scheduler_code(aiida_local_code_factory):
-    """Get a nims_scheduler code.
-    """
-    nims_scheduler_code = aiida_local_code_factory(
-        executable='diff', entry_point='nims_scheduler')
-    return nims_scheduler_code
